0c2acbaeda5aa64b6fa66faac818ca6da6aa4f52
IKolmykov <zabyz1337@gmail.com>
2026-04-20 10:13:53 +0200

    add discount banner section

---
+++ src/components/DiscountBanner/DiscountBanner.jsx
@@ -0,0 +1,28 @@
+import styles from "./DiscountBanner.module.css";
+
+function DiscountBanner() {
+  return (
+    <section className={styles.banner}>
+      <div className="container">
+        <div className={styles.box}>
+          <h2 className={styles.title}>5% off on the first order</h2>
+
+          <div className={styles.wrapper}>
+            <div className={styles.left}></div>
+
+            <div className={styles.right}>
+              <form className={styles.form}>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Phone number" />
+                <input type="email" placeholder="Email" />
+                <button type="submit">Get a discount</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default DiscountBanner;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,22 @@
 import styles from "./HomePage.module.css";
+import DiscountBanner from "../../components/DiscountBanner/DiscountBanner";
 
 function HomePage() {
   return (
-    <section className={styles.home}>
-      <div className="container">
-        <div className={styles.hero}>
-          <h1 className={styles.title}>Amazing Discounts on Pets Products!</h1>
-          <button className={styles.button}>Check out</button>
+    <>
+      <section className={styles.home}>
+        <div className="container">
+          <div className={styles.hero}>
+            <h1 className={styles.title}>
+              Amazing Discounts on Pets Products!
+            </h1>
+            <button className={styles.button}>Check out</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <DiscountBanner />
+    </>
   );
 }
 
