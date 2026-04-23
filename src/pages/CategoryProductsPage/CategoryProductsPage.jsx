7309166b05f59eb795cf24ed50aa70bcf3300d5a
IKolmykov <zabyz1337@gmail.com>
2026-04-23 10:45:42 +0200

    use ProductCard in category products page

--- src/pages/CategoryProductsPage/CategoryProductsPage.jsx
+++ src/pages/CategoryProductsPage/CategoryProductsPage.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axiosInstance from "../../api/axiosInstance";
 import styles from "./CategoryProductsPage.module.css";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 function CategoryProductsPage() {
   const { id } = useParams();
@@ -45,59 +46,9 @@ function CategoryProductsPage() {
         <h1 className={styles.title}>{categoryData.category.title}</h1>
 
         <div className={styles.grid}>
-          {categoryData.data.map((product) => {
-            const hasDiscount =
-              product.discont_price !== null &&
-              product.discont_price !== undefined;
-
-            const discountPercent = hasDiscount
-              ? Math.round(
-                  ((product.price - product.discont_price) / product.price) *
-                    100,
-                )
-              : 0;
-
-            return (
-              <Link
-                key={product.id}
-                to={`/products/${product.id}`}
-                className={styles.card}
-              >
-                <div className={styles.imageWrapper}>
-                  {hasDiscount && (
-                    <div className={styles.badge}>-{discountPercent}%</div>
-                  )}
-
-                  <img
-                    src={`http://localhost:3333/${product.image}`}
-                    alt={product.title}
-                    className={styles.image}
-                  />
-                </div>
-
-                <div className={styles.content}>
-                  <p className={styles.name}>{product.title}</p>
-
-                  <div className={styles.prices}>
-                    {hasDiscount ? (
-                      <>
-                        <span className={styles.currentPrice}>
-                          ${product.discont_price}
-                        </span>
-                        <span className={styles.oldPrice}>
-                          ${product.price}
-                        </span>
-                      </>
-                    ) : (
-                      <span className={styles.currentPrice}>
-                        ${product.price}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </Link>
-            );
-          })}
+          {categoryData.data.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     </section>
