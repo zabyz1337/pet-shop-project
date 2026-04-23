5a090473763312dcaffd0acb7a34d1fa957e6e65
IKolmykov <zabyz1337@gmail.com>
2026-04-23 09:29:12 +0200

    create 404 page and add image

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
+import dogImg from "../../assets/images/404-dog.png";
 
 function NotFoundPage() {
   return (
-    <div className="container pageStub">
-      <h1>404 Page Not Found</h1>
-      <Link to="/">Go Home</Link>
-    </div>
+    <section className={styles.page}>
+      <div className="container">
+        <div className={styles.content}>
+          <div className={styles.visual}>
+            <span className={styles.four}>4</span>
+            <img src={dogImg} alt="Dog" className={styles.dog} />
+            <span className={styles.four}>4</span>
+          </div>
+
+          <h1 className={styles.title}>Page Not Found</h1>
+
+          <p className={styles.text}>
+            We are sorry, the page you requested could not be found.
+          </p>
+
+          <Link to="/" className={styles.button}>
+            Go home
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
 
